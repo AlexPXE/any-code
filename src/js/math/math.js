edf73a56934a68861c9ed64645d5ea378b0b70c9
Alex <devpublicalex@gmail.com>
2022-06-14 04:25:37 +0300

    New names for some function. Now the names of functions thet work with bigint type end with 'Big'

--- src/js/math/math.js
+++ src/js/math/math.js
@@ -47,10 +47,10 @@ function random(min, max) {
  * @param {bigint} p Exponent.Default value is BIG_TWO. BigInt only. 
  * @returns {bigint} Result.
  */ 
- function fastPow(n, p = BIG_TWO) {        
+ function fastPowBig(n, p = BIG_TWO) {        
 
     if(typeof p !== 'bigint' && typeof n !== 'bigint') {        
-        throw new Error('Function fastPow() only accepts BigInt numbers.');
+        throw new Error('Function fastPowBig() only accepts BigInt numbers.');
     }
 
     if(p === BIG_ZERO) {
@@ -80,7 +80,7 @@ function random(min, max) {
 }
 
 /**
- * The modExp() function is a cyclic function that calculates the modular exponentiation of a number. 
+ * The modExpBig() function is a cyclic function that calculates the modular exponentiation of a number. 
  * Works with [BigInt only](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt).
  * 
  * @param {bigint} a Number thet will be raised to the power. BigInt only.
@@ -90,14 +90,14 @@ function random(min, max) {
  * @throws {TypeError} If a, p or m are not bigint.
  * @throws {Error} If a, p or m is negative.
  */
-function modExp(a, p, m) {   //modExp(12n, 12n, 13n)
+function modExpBig(a, p, m) {   //modExpBig(12n, 12n, 13n)
     
     if(typeof a !== 'bigint' && typeof p !== 'bigint' && typeof m !== 'bigint') {        
-        throw new TypeError('Function fastPow() only accepts BigInt numbers.');
+        throw new TypeError('Function fastPowBig() only accepts BigInt numbers.');
     }
 
     if(a < 0n || p < 0n || m < 0n) {
-        throw new Error('Function modExp() only accepts positive numbers.');
+        throw new Error('Function modExpBig() only accepts positive numbers.');
     }    
     
     let x = a;
@@ -121,7 +121,7 @@ function modExp(a, p, m) {   //modExp(12n, 12n, 13n)
 }
 
 /**
- * The modExpR() function is a recursive function that calculates the modular exponentiation of a number.
+ * The modExpBigR() function is a recursive function that calculates the modular exponentiation of a number.
  * Works with [BigInt only](@linkhttps://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt).
  * If number, exponent and modulus are not positive, then returns uncorrect result.
  * 
@@ -130,16 +130,16 @@ function modExp(a, p, m) {   //modExp(12n, 12n, 13n)
  * @param {bigint} m Modulus.
  * @returns {bigint} Result of modular exponentiation.
  */
- function modExpR(a, p, m) {
+ function modExpBigR(a, p, m) {
     if(p === BIG_ZERO) {
         return BIG_ONE;
     }
     
     if(p % BIG_TWO === BIG_ZERO) {       
-        return modExpR(a * a % m, p / BIG_TWO, m);       
+        return modExpBigR(a * a % m, p / BIG_TWO, m);       
     }
 
-    return (a * modExpR(a, p - BIG_ONE, m)) % m;
+    return (a * modExpBigR(a, p - BIG_ONE, m)) % m;
 }
 
 /**
@@ -154,15 +154,15 @@ function gcd(n, m) {
 }
 
 /**
- * The gcd() function calculates the [greatest common divisor of two numbers.](https://en.wikipedia.org/wiki/Greatest_common_divisor)
+ * The gcdBig() function calculates the [greatest common divisor of two numbers.](https://en.wikipedia.org/wiki/Greatest_common_divisor)
  * BigInt only. To work with integers, [use gcd().]{@link module:math~gcd}} 
  * 
  * @param {bigint} n First number.
  * @param {bigint} m Second number.
  * @returns {bigint} gcd of n and m.
  */
-function gcdBigInt(n, m) {
-    return m === BIG_ZERO ? n : gcdBigInt(m, n % m);
+function gcdBig(n, m) {
+    return m === BIG_ZERO ? n : gcdBig(m, n % m);
 }
 
 
@@ -190,10 +190,10 @@ function eulerst(n) {
  * @param {bigint} n Number to calculate Euler's totient function.
  * @returns {bigint} Euler's totient function of n.
  */
-function etBigInt(n) {
+function eulerstBig(n) {
     let d = BIG_ZERO;
     for(let i = BIG_ONE; i < n; i++) {
-        if ( gcdBigInt(n, i) === BIG_ONE) {           
+        if ( gcdBig(n, i) === BIG_ONE) {           
             d++;
         }
     }
@@ -201,16 +201,16 @@ function etBigInt(n) {
 }
 
 /**
- * The randomBigInt() returns a random bigint number between min and max (iclusive of min, but not max). * 
+ * The randomBig() returns a random bigint number between min and max (iclusive of min, but not max). * 
  * 
  * @param {bigint} min Minimal value of the random number.
  * @param {bigint} max Maximal value of the random number.
  * @returns {bigint} Random bigint number.
  */
-function randomBigInt(min, max) {    
+function randomBig(min, max) {    
 
     if(typeof max !== 'bigint' || max < BIG_ZERO) {        
-        throw new Error('Function randomBigInt() only accepts positive numbers of type bigint.');
+        throw new Error('Function randomBig() only accepts positive numbers of type bigint.');
     }
     
     return BigInt( max.toString()
@@ -227,32 +227,32 @@ function randomBigInt(min, max) {
 }
 
 /**
- * The fermaTestR() calculates the Fermat's primality test of a number. Recursive version.
+ * The fermaTestBigBigR() calculates the Fermat's primality test of a number. Recursive version.
  * [a**(p-1) % p = 1 for all a < p.](https://en.wikipedia.org/wiki/Fermat_primality_test)
  * 
  * @param {bigint} p Estimated prime number.
  * @param {number} [checks = FERMAS_TEST_DEFAULT_CHECKS] Quantity of checks. The more checks, the more accurate the result, but the longer the calculation time.
  * @returns {boolean} True if the number is prime, false if the number is not prime.
  */
-function fermaTestR(p, checks = FERMAS_TEST_DEFAULT_CHECKS) {
-    const a = randomBigInt(FERMAS_TEST_MIN_RANDOM_NUMBER, p);   
+function fermaTestBigBigR(p, checks = FERMAS_TEST_DEFAULT_CHECKS) {
+    const a = randomBig(FERMAS_TEST_MIN_RANDOM_NUMBER, p);   
 
     if(checks > 0) {
-        return modExp(a, p - BIG_ONE, p) !== BIG_ONE ? false  : fermaTestR(p, --checks);
+        return modExpBig(a, p - BIG_ONE, p) !== BIG_ONE ? false  : fermaTestBigBigR(p, --checks);
     }
     
     return true;
 }
 
 /**
- * The fermaTest() calculates the Fermat's primality test of a number. Loop version.
+ * The fermaTestBig() calculates the Fermat's primality test of a number. Loop version.
  * [a**(p-1) % p = 1 for all a < p.](https://en.wikipedia.org/wiki/Fermat_primality_test)
  * 
  * @param {bigint} p Estimated prime number.
  * @param {number} [checks = FERMAS_TEST_DEFAULT_CHECKS] Quantity of checks. The more checks, the more accurate the result, but the longer the calculation time.
  * @returns {boolean} True if the number is prime, false if the number is not prime.
  */
-function fermaTest(p, checks = FERMAS_TEST_DEFAULT_CHECKS) {
+function fermaTestBig(p, checks = FERMAS_TEST_DEFAULT_CHECKS) {
     
     if(p < 2n) {
         return false;
@@ -260,8 +260,8 @@ function fermaTest(p, checks = FERMAS_TEST_DEFAULT_CHECKS) {
 
     for(let i = 0; i < checks; i++) {
         
-        const a = randomBigInt(FERMAS_TEST_MIN_RANDOM_NUMBER, p);        
-        if(modExp(a, p - 1n, p) !== 1n) {
+        const a = randomBig(FERMAS_TEST_MIN_RANDOM_NUMBER, p);        
+        if(modExpBig(a, p - 1n, p) !== 1n) {
             return false;
         }
 
@@ -269,11 +269,8 @@ function fermaTest(p, checks = FERMAS_TEST_DEFAULT_CHECKS) {
     return true;
 }
 
-
-
-
 /**
- * The gcdExBigInt() computes, in addition to the gcd, the coefficients **a** and **b** of the equation __a*x + b*y = gcd(x, y)__.
+ * The gcdExBig() computes, in addition to the gcd, the coefficients **a** and **b** of the equation __a*x + b*y = gcd(x, y)__.
  * See [the Extended Euclidean algorithm](https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm) for more information.  
  * **BigInt only.**
  * 
@@ -281,24 +278,22 @@ function fermaTest(p, checks = FERMAS_TEST_DEFAULT_CHECKS) {
  * @param {bigint} b Second number.
  * @returns {bigint[]} Array with the coefficients a and b of the equation a*x + b*y = gcd(x, y) and the gcd of a and b ([x, y, gcd(a, b)]).
  */
-function gcdExBigInt(a, b) {
+function gcdExBig(a, b) {
     if (b === BIG_ZERO) {
         return [BIG_ONE, BIG_ZERO, a];
     }
     const [q, r] = [a / b, a % b];
-    const [s, t, g] = gcdExBigInt(b, r);
+    const [s, t, g] = gcdExBig(b, r);
     return [t, s - q * t, g];    
 }
-
-
 
 
 //console.log(test(48, 23));
 
 
 
-//console.log('r', fermaTestR(21377n, 2));
-//console.log('d', fermaTest(21377n, 2));
+//console.log('r', fermaTestBigBigR(21377n, 2));
+//console.log('d', fermaTestBig(21377n, 2));
 //console.log('e', eulerst(21377));
 
 
@@ -307,13 +302,13 @@ function gcdExBigInt(a, b) {
 
 export { 
     random, 
-    fastPow, 
-    modExp, 
-    modExpR, 
+    fastPowBig, 
+    modExpBig, 
+    modExpBigR, 
     gcd, 
     eulerst, 
-    randomBigInt, 
-    fermaTestR, 
-    fermaTest,
-    gcdExBigInt
+    randomBig, 
+    fermaTestBigBigR, 
+    fermaTestBig,
+    gcdExBig
 };

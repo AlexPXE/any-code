75cb2fb6471be8768700a5933fad6bcfd8de3f01
Alex <devpublicalex@gmail.com>
2022-07-17 00:43:11 +0300

    utility: isVoid, customJSON added

--- src/js/utility/utility.js
+++ src/js/utility/utility.js
@@ -8,7 +8,27 @@
  */
 
 
-const {isVoid} = new function() {
+const TYPE_HANDLERS = [
+    [
+        'Map', 
+        value => [...value], 
+        value => new Map(value)
+    ],
+
+    [
+        'Set',
+        value => [...value],
+        value => new Set(value)
+    ],
+
+    [
+        'BigInt',
+        value => value.toString(),
+        value => BigInt(value)
+    ]
+];
+
+ const {isVoid} = new function() {
     const voidValues = new Set([undefined, null, NaN, Infinity, -Infinity]);
 
     this.isVoid = (value) => {
@@ -16,92 +36,108 @@ const {isVoid} = new function() {
     }
 };
 
-const {JSONencode} = new function() {
+const customJSON = new function() {
+    
 
-    const {handler} = new function() {
-
-        function ValueWrapp(sType, value) { /*Wrapper for the value being encoded*/
-            this.JSONdecodeType = sType;
-            this.value = value;
-        }
+    function ValueWrapp(sType, value) { /*Wrapper for the value being encoded*/
+        this.JSONdecodeType = sType;
+        this.JSONdecodeValue = value;
+    }
+    
+    function ValueConverter(arr) { 
         
-        const typesHandlers = { /*Processed types (class construtors names) and handlers*/
-            'Map': value => new ValueWrapp('Map', [...value]),
-            'Set': value => new ValueWrapp('Set', [...value]),
-            'BigInt': value => new ValueWrapp('BigInt', `${value.toString()}`),
-        };
-        
-        this.handler = value => { /*Main type handler */
-            return typesHandlers[value?.constructor.name]?.(value) || value; /*The name of the constructor is the name of the handler*/
-        }
-    };
+        const handlers = arr.reduce((acc, [name, e, d]) => acc.set(name, {e, d}), new Map());
 
-    const cbFactory = replacer => {     /*JSON.stringify callback factory*/
-
-        if( isVoid(replacer) ) {        /*If there is no replacer*/
-            return function(key, value) {
-                return handler(value);
-            };
-        }
-
-        if( typeof replacer === 'function' ) {
-            return function(key, value) {
-                return handler( replacer(key, value) );
-            };
-        }
-
-        if(replacer?.[Symbol.iterator]) {    /*If replacer is an iterable*/
-            const inclProps = new Set(replacer);
-
-            return function(key, value) {
-                
-                console.log(key, value);
-                if( inclProps.has(key) ) {
-                    return handler(value);
-                } 
-
-                return '';
+        this.convert = value => {
+            const type = value?.constructor?.name;
+            
+            if( handlers.has(type) ) {                
+                return new ValueWrapp(type, handlers.get(type).e(value));
             }
+
+            return  value;
         }        
+
+        this.toOriginal = value => {
+            if(value?.JSONdecodeType) {
+                return handlers.get(value.JSONdecodeType).d(value.JSONdecodeValue);
+            }
+
+            return value;
+        }
     }
+   
+    let {convert, toOriginal} = new ValueConverter(TYPE_HANDLERS); /*Processed types (class construtors names) and handlers*/
 
-    this.JSONencode = (value, replacer, space) => {        
-        return JSON.stringify(value, cbFactory(replacer), space);
-    }
-};
+    /**
+     * Encode method.
+     */
+    this.encode = (function() {        
+    
+        const cbFactory = replacer => {     /*JSON.stringify callback factory*/
+    
+            if( isVoid(replacer) ) {        /*If there is no replacer*/
+                return function(key, value) {
+                    return convert(value);
+                };
+            }
+    
+            if( typeof replacer === 'function' ) {
+                return function(key, value) {
+                    return convert( replacer(key, value) );
+                };
+            }
+    
+            if(replacer?.[Symbol.iterator]) {    /*If replacer is an iterable*/
+                const inclProps = new Set(replacer)
+                    .add("")
+                    .add("JSONdecodeType")
+                    .add("JSONdecodeValue");
+    
+                return function(key, value) {
+                    
+                    if( inclProps.has(key) || Array.isArray(this) ) {
+                        return convert(value);
+                    } 
+    
+                    return undefined;
+                }
+            }        
+        }
 
+        return (value, replacer, space) => {        
+            return JSON.stringify(value, cbFactory(replacer), space);
+        }
 
+    })();
 
+    /**
+     * Decode method.
+     */
+    this.decode = (function() {
+        
+        const cbFactory = reviewer => {
 
+            if( isVoid(reviewer) ) {
+                return function(key, value) {
+                    return toOriginal(value);
+                };
+            }
 
+            if( typeof reviewer === 'function' ) {
+                return function(key, value) {
+                    return reviewer(key, toOriginal(value) );
+                }
+            }
+        };
 
-const bb = {
-    a: {
-        b: 1,
-        c: 2,
-    },
-    b: 23,
-    c: 3,
-    d: 4,
-    e: new Map([
-        ['f', 5],
-        ['g', 6],
-        ['h', 7],
-    ]),
+        return (value, reviewer) => {        
+            return JSON.parse(value, cbFactory(reviewer));
+        }
+    })();    
     
 };
 
 
-
-//console.log(JSONencode(bb, ['a', 'b', 'c']) );
-
-console.log(JSON.stringify(bb, function(key, value) {
-    console.log(key, typeof key);
-    if(key === 'b') {
-        console.log(value);
-        return value;
-    }
-    return value;
-},2) );
-
+export {customJSON, isVoid};
 
